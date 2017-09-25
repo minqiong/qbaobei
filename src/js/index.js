78475712a66410584ea5eb5f39d1a6ab16275c47
minqiong <1140957252@qq.com>
2017-09-25 18:07:41 +0800

    首页主体内容完善

--- src/js/index.js
+++ src/js/index.js
@@ -35,4 +35,44 @@ require(['jquery','mincar','common'],function($,min){
     }).mouseleave(function(){
     	$(this).animate({width:13}).removeAttr('style').find('a').css({color:'#ffebeb'});
     });
+    // 怀孕模块tal标签切换动画
+    // 默认给第一个tab li加样式
+    // 左边
+    $('.main_con_ll li').first().addClass('active');
+    $('.main_con_ll li').click(function(){
+    	$('.main_con_ll li').eq($(this).index()).addClass('active').siblings().removeClass('active');
+    	$('.main_1_tab1').eq($(this).index()).animate({opacity:1,left:0}).siblings().animate({opacity:0,left:'100%'});
+    	console.log($(this).index());
+    });
+    // 右边
+    $('.main_con_rt li').first().addClass('active');
+    $('.main_1_rb_box').width($('.main_1_rb_box ul').width()*2)
+    $('.main_con_rt li').click(function(){
+    	$('.main_1_rb_box').animate({left:-$('.main_1_rb_box ul').width()*$(this).index()});
+    	$('.main_con_rt li').eq($(this).index()).addClass('active').siblings().removeClass('active');
+    });
+    // 工具部分动画
+    var $dl_len=$('.rb_dl').find('dd').length;
+    var $dl_width=$('.rb_dl dl dd').width();
+    $('.rb_dl dl').width($dl_width*$dl_len);
+    var $dl_index=0;
+    $('.rb_dl').on('click','.btn_prev,.btn_next',function(){
+    	this.className==='btn_prev'?$dl_index++:$dl_index--;
+    	$('.rb_dl dl').animate({left:$dl_len/2*$dl_width*$dl_index});
+    })
+    // $('.rb_dl .btn_prev').click(function(){
+    // 	$dl_index++;
+    // 	console.log($dl_index)
+    // 	$('.rb_dl dl').animate({left:$dl_len/2*$dl_width*$dl_index});
+    // });
+    // $('.rb_dl .btn_next').click(function(){
+    // 	$dl_index--;
+    // 	$('.rb_dl dl').animate({left:$dl_len/2*$dl_width*$dl_index});
+    // });
+    // 排行榜动画
+    $('.main_5top li').eq(0).find('p').first().css({display:'none'}).next().addClass('active');
+    $('.main_5top li').mouseenter(function(){
+    	console.log($(this).index());
+    	$(this).eq($(this).index()).find('p').first().css({display:'none'}).next().addClass('active').siblings().find('p').first().css({display:'block'}).next().removeClass('active');
+    });
 });
